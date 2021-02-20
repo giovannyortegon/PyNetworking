99aed1db0bb11bca751128f9fd974870c3e0fb85
Skillhh <ortegon.giovanny@hotmail.com>
2021-02-20 00:00:37 -0500

    [ADD] socket errors

---
+++ Chp01/7-socket_errors.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# Python Network Programming Cookbook, Second Edition -- Chapter - 1
+# This program is optimized for Python 2.7.12 and Python 3.5.2.
+# It may run on any other version with/without modifications.
+import sys
+import socket
+import argparse
+
+def main():
+    # setup argument parsing
+    parser = argparse.ArgumentParser(description='Socket Error Examples')
+    parser.add_argument('--host', action="store", dest="host",
+                         required=False)
+    parser.add_argument("--port", action="store", dest="port",
+                        required=False)
+    parser.add_argument("--file", action="store", dest="file",
+                        required=False)
+
+    given_args = parser.parse_args()
+    host = given_args.host
+    port = int(given_args.port)
+    filename = given_args.file
+
+    # Fist try-except block -- create socket
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error as e:
+        print("Error creating socket: %s" % e)
+        sys.exit(1)
+    # Second try-except block -- connect to given host/port
+    try:
+        s.connect((host, port))
+    except socket.gaierror as e:
+        print("Address-related error connection to server: %s" % e)
+        sys.exit(1)
+    except socket.error as e:
+        print("Connetion error: %s" %e)
+        sys.exit(1)
+    # Third try-except block -- sending data
+    try:
+        msg = "GET %s HTTPS/1.0\r\n\r\n" % filename
+        s.sendall(msg.encode('utf-8'))
+    except socket.error as e:
+        print("Error sending data: %s" % e)
+        sys.exit(1)
+        # Fourth try-except block -- waiting to recive data from remote host
+        try:
+            buf = s.recv(2048)
+        except socket.error as e:
+            print("Error receiving data: %s" % e)
+            sys.exit(1)
+        if not len(buf):
+            sys.exit(0)
+        # write the received data
+        sys.stdout.write(buf.decode('utf-8'))
+
+if __name__ == '__main__':
+    main()
