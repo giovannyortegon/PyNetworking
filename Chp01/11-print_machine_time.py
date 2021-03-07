c3602225506182c32aca27ed3421d85895d5d4d8
Skillhh <ortegon.giovanny@hotmail.com>
2021-03-07 17:17:45 -0500

    [ADD] creating TCP client/Server echo

---
+++ Chp01/11-print_machine_time.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import ntplib
+from time import ctime
+
+def print_time():
+    ntp_client = ntplib.NTPClient()
+    response = ntp_client.request("pool.ntp.org")
+    print(ctime(response.tx_time))
+
+if __name__ == '__main__':
+    print_time()
